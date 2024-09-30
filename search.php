2f33f21bef389ab4ad53bf7ec534ef3a492b9139
IVEN2022 <ivenmsf@gmail.com>
2024-09-30 03:40:59 +0300

    123

--- search.php
+++ search.php
@@ -601,5 +601,23 @@ function displayResults(results) {
     });
     </script>
 
+    <!-- موديول تفاصيل الدرس -->
+    <div class="modal fade" id="lessonModal" tabindex="-1" aria-labelledby="lessonModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="lessonModalLabel">تفاصيل الدرس</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <!-- سيتم ملء المحتوى ديناميكياً -->
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">إغلاق</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
